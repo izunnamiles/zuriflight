3f95043851b08cf4fc419baa36de937463141ffc
izunna <izuchukwu@seamlesshr.com>
2022-06-25 20:57:15 +0100

    update route

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -1,11 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router.get('/', controller.findAll)
 router.get('/flight/:id', controller.findOne)
-router.get('/flights', controller.findAll)
 router.post('/flight', controller.create)
 router.patch('/flight/:id', controller.update)
 router.delete('/flight/:id', controller.delete)
